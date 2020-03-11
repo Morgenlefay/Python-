4c8d2dcaa593cd36893e1606d767728645c7d220
Zhiyuan Luo <49680424+Morgenlefay@users.noreply.github.com>
2020-03-11 17:06:51 +0800

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -8,12 +8,13 @@ file = open('WordCloud/红楼梦.txt','rt', encoding = 'utf-8')
 text = file.read()
 file.close()
 
-ls = jieba.lcut(text) #分词
+ls = jieba.cut(text) #分词
 txt = " ".join(ls)
 
 stopwords = {'我们':0, '你们':0, '他们':0, '那里':0, '这里':0, '一个':0} #噪声词
 color_mask =imageio.imread("WordCloud/love.jpeg")
 
+w = wordcloud.WordCloud()
 w = wordcloud.WordCloud(font_path = 'WordCloud/msyh.ttf',
                         width = 1000,
                         height = 700,
